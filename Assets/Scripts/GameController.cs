e6b5b5be532566859090a31438c3418252dbb322
davidwfranco <davidwfranco@gmail.com>
2017-11-18 01:46:02 -0200

    First commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public static GameController instance;
+	public Camera cam;
+
+    public Vector2 upperCorner;
+    public Vector2 targeCamtWidth;
+    public float maxWidth;
+    public float maxHeigth;
+
+    public bool gameOver;
+
+    // Use this for initialization && guarantee that there's
+    // only one instance of the GameController
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        upperCorner = new Vector2(Screen.width, Screen.height);
+        targeCamtWidth = GameController.instance.cam.ScreenToWorldPoint(upperCorner);
+        maxWidth = targeCamtWidth.x;
+        maxHeigth = targeCamtWidth.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody2D rdb2d;
+
+    private float maxW;
+    private float maxH;
+
+	// Use this for initialization
+	void Start () {
+		rdb2d = GetComponent<Rigidbody2D> ();
+        maxW = GameController.instance.maxWidth;
+        maxH = GameController.instance.maxHeigth;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("up")) 
+		{
+			rdb2d.transform.position = new Vector2 (0, maxH - (maxH / 3));
+            Debug.Log(maxH);
+		} 
+		else if (Input.GetKeyDown("down"))
+        {
+            rdb2d.transform.position = new Vector2(0, -(maxH - (maxH / 3)));
+        }
+        else if (Input.GetKeyDown("left"))
+        {
+            rdb2d.transform.position = new Vector2(-(maxW - (maxW / 3)), 0);
+        }
+        else if (Input.GetKeyDown("right"))
+        {
+            rdb2d.transform.position = new Vector2((maxW - (maxW / 3)), 0);
+        }
+        else if (Input.GetKeyUp("up") || Input.GetKeyUp("down") || Input.GetKeyUp("left") || Input.GetKeyUp("right"))
+        {
+            rdb2d.transform.position = new Vector2(0, 0);
+        }
+	}
+}
